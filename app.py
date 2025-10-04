14badd9ee6a0bc6f1ecce173371912dcc77cb7e6
Maanasi.S <142606403+MaanasiS@users.noreply.github.com>
2025-10-04 14:31:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,8 @@ def delete_note(note_id):
 
 if __name__ == "__main__":
     init_db()  # ensures database + table are created
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000)
+
 
 
 
